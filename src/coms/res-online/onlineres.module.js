52fec063048ea67cea880a1e699a96a8cce51eaf
lgzhang <zhanglg@tfedu.net>
2016-03-28 15:09:58 +0800

    fix online res

--- src/coms/res-online/onlineres.module.js
+++ src/coms/res-online/onlineres.module.js
@@ -38,7 +38,47 @@
 				// 筛选 主controller 
 				// 变量共享
 				$scope.VM = {};
-					
+				
+				$scope.slides = [
+				{
+					title: "荷塘月色-课件1",
+					type:'img',
+					src: "http://7xpwjy.com1.z0.glb.clouddn.com/780700105_20160224113433.png"
+				},{
+					title: "荷塘月色-图片",
+					type:'pdf',
+					src: "http://chat.tfedu.net/res/fe-tiku.pdf"
+				},{
+					title: "荷塘月色-pdf",
+					type:'media',
+					src: "http://m.tfedu.net/book/ereader/"
+				},{
+					title: "荷塘月色-多媒体",
+					type:'img',
+					src: "http://7xpwjy.com1.z0.glb.clouddn.com/780700105_20160224113433.png"
+				}]
+				
+				$scope.selectRes = function(index) {
+					var tpl = '';
+					switch($scope.slides[index].type)
+					{
+					case "img":
+					  tpl = "<img src='" +$scope.slides[index].src + "' />"
+					  $('#res-slide-content').html(tpl);
+					  break;
+					case "pdf":
+					  tpl = "<iframe width='100%' height='700px' src='" +$scope.slides[index].src + "' ></iframe>"
+					  $('#res-slide-content').html(tpl);
+					  break;
+					case "media":
+					  tpl = "<iframe width='100%' height='700px' src='" +$scope.slides[index].src + "' ></iframe>"
+					  $('#res-slide-content').html(tpl);
+					  break;
+					default:
+					   tpl = "<img src='" +$scope.slides[0].src + "' />"
+					  $('#res-slide-content').html(tpl);
+					}
+				}
 			}
 		])
 }());
