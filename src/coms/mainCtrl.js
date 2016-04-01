371d9486b6b7f13295c63d497004a6a6a446d839
lgzhang <zhanglg@tfedu.net>
2016-04-01 16:28:44 +0800

    目录

--- src/coms/mainCtrl.js
+++ src/coms/mainCtrl.js
@@ -1,8 +1,13 @@
-angular.module('webApp')
-	.controller("MainCtrl", ['$scope', '$rootScope', '$window', '$localStorage',
-	function($scope, $rootScope, $window,  $localStorage) {
-		// 监听 目录树更改
-		$scope.$on("currentTreeId",function(e, d) {
-			$scope.$broadcast('currentTreeIdUpdate', d);		
-		})
-	}]);
+(function() {
+	'use strict';
+	// 主controller
+	angular.module('webApp')
+		.controller("MainCtrl", ['$scope', '$rootScope', '$window', '$localStorage',
+			function($scope, $rootScope, $window, $localStorage) {
+				// 监听 目录树更改
+				$scope.$on("currentTreeId", function(e, d) {
+					$scope.$broadcast('currentTreeIdUpdate', d);
+				})
+			}
+		]);
+}());
