e3ad5d87420f9f99f4000f66d0e2ef1e9fac3170
huhu213 <15501003885@163.com>
2016-04-01 18:15:35 +0800

    user info

--- src/coms/personal-center/center.module.js
+++ src/coms/personal-center/center.module.js
@@ -1,43 +1,3 @@
-<<<<<<< HEAD
-/**
- * 个人中心模块
- */
-(function() {
-	'use strict';
-	//Module configuration
-	angular.module('webApp.coms.personalcenter')
-		.config(['$stateProvider',
-			function($stateProvider) {
-				$stateProvider
-					.state('personalcenter', {
-						url: '/personalcenter',
-						views: {
-							'content@': {
-								templateUrl: '/coms/personal-center/views/personal-center.html',
-								controller: 'PrepareController'
-							},
-							'header@': {
-								templateUrl: '/coms/layout/header/header3.html',
-								controller: 'LayoutController'
-							},
-							'footer@': {
-								templateUrl: '/coms/layout/footer/footer.html'
-							}
-						}
-					})
-			}
-		])
-		.controller("personalCenterController", ['$scope', '$stateParams', '$state', '$location', '$http',
-			function($scope, $stateParams, $state, $location, $http) {
-				$scope.maxSize = 3;
-				$scope.bigTotalItems = 175;
-				$scope.bigCurrentPage = 1;
-				
-//				
-			}
-		])
-}())
-=======
 /**
  * 个人中心模块
  */
@@ -76,4 +36,3 @@
 			}
 		])
 }());
->>>>>>> 97a23b96689010a8c8f138eef650a1691123dddf
--- src/coms/settings/settings.module.js
+++ src/coms/settings/settings.module.js
@@ -31,43 +31,152 @@
 				return $resource(window.BackendUrl+"/resRestAPI/v1.0/users/:userid",{
                     userid: '@_id'
                 },{
-					getUser: {method: "GET",url: window.BackendUrl + "/resRestAPI/v1.0/users/"}
+					getUser: {method: "GET",url: window.BackendUrl + "/resRestAPI/v1.0/users/"},
+					getAllTerms: {method: "GET", url: window.BackendUrl + "/resRestAPI/v1.0/terms"},
+					getAllSubjects: {method: "GET", url: window.BackendUrl + "/resRestAPI/v1.0/subjects"},
+					setUserInfo: {method: "POST", url: window.BackendUrl + "/resRestAPI/v1.0/users/:userid", params: {userid: '@userid'}},
 				})
 			}
 		])
 		.controller("SettingsController", ['$scope', '$stateParams', '$state', '$location', '$uibModal', 'User','$localStorage',
 			function($scope, $stateParams, $state, $location, $uibModal,User,$localStorage) {
-				//测试
+				
+				//当前学段
+				$scope.currentTerm = {
+				}
+				//学科选择列表
+				$scope.subjects = [];
+				//选中的学段
+				$scope.selectedTerm;
+				//选中的学科
+				$scope.selectedSubject;
+				
+				
+				
+				//获取用户信息
 				User.get({
 					userid: $localStorage.authUser.userId
 				}, function(data){
-					console.log(data.data)
+					var info = data.data;
+					console.log(data);
+					$scope.userName = info.userName;
+					$scope.schoolName = info.schoolName;
+					$scope.trueName = info.trueName;
+					$scope.sexSelected = info.male;
+					$scope.userImage = info.userImage;
+					//获取学段信息
+					User.getAllTerms({
+						
+					}, function(data) {
+						$scope.terms = data.data;
+						if($scope.terms) {
+							for(var i = 0; i < $scope.terms.length; i ++) {
+								if($scope.terms[i].name == info.termName) {
+									$scope.terms[i].active = true;
+									$scope.selectedTerm = $scope.terms[i];
+								}
+								else $scope.terms[i].active = false;
+							}
+							User.getAllSubjects({
+								termId: $scope.selectedTerm.id
+							}, function(data) {
+								console.log(data.data);
+								$scope.subjects = data.data;
+								if($scope.subjects && $scope.subjects.length > 0) {
+									if(info.subjectNames) {
+										for(var i = 0; i < $scope.subjects.length; i ++) {
+											if($scope.subjects[i].name == info.subjectNames) {
+												$scope.subjects[i].active = true;
+												$scope.selectedSubject = $scope.subjects[i];
+											}
+											else $scope.subjects[i].active = false;
+										}
+									}
+									else {
+										$scope.subjects[0].active = true;
+										$scope.selectedSubject = $scope.subjects[0];
+									}
+								}
+							})
+						}
+					})
 				})
-				
 				
 				
 				// 变量共享
 				$scope.VM = {};
-				var test="test";
-				$scope.VM.avatarModal = $uibModal.open({
-			      	templateUrl: 'avatar.html',
-			      	controller: 'avatarInstanceController',
-			      	size: '',
-			      	resolve: {
-			      		test: function() {
-			      			return test;
-			      		}
-			      		}
-	    			});	
+				
+				//修改信息modifyInfoJson
+				var infojson = {
+					
+				}
+				
+				$scope.selectTerm = function(index) {
+					for(var i = 0; i < $scope.terms.length; i ++) $scope.terms[i].active = false;
+					$scope.terms[index].active = true;
+					$scope.selectedTerm = $scope.terms[index];
+					User.getAllSubjects({
+						termId: $scope.selectedTerm.id
+					}, function(data) {
+						console.log(data.data);
+						$scope.subjects = data.data;
+						if($scope.subjects && $scope.subjects.length > 0) {
+							$scope.subjects[0].active = true;
+							$scope.selectedSubject = $scope.subjects[0];
+						}
+					})
+				}
+				
+				
+				$scope.selectSubject = function(index) {
+					for(var i = 0; i < $scope.subjects.length; i ++) $scope.subjects[i].active = false;
+					$scope.subjects[index].active = true;
+					$scope.selectedSubject = $scope.subjects[index];
+				}
+				
+				$scope.setMale = function(male) {
+					$scope.sexSelected = male;
+				}
+				
+				$scope.saveUserInfo = function() {
+					var saveSuccess = "保存成功";
+					var btnSave = "确定";
+					User.setUserInfo({
+						userid: $localStorage.authUser.userId,
+						userImage: $scope.userImage,
+						trueName: $scope.trueName,
+						termId: $scope.selectedTerm.id,
+						subjectId: $scope.selectedSubject.id,
+						male: $scope.sexSelected,
+						_method: "PATCH"
+					}, function(data) {
+						if(data) {
+							$scope.VM.messageModal = $uibModal.open({
+								templateUrl: "message.html",
+								controller: "messageInstanceController",
+								size: 'sm',
+								resolve: {
+									messageContent: function() {
+										return saveSuccess;
+									},
+									buttonContent: function() {
+										return btnSave;
+									}
+								}
+							})
+						}
+					})
+				}
+				
+				
+				
+				
 				$scope.changeAvatar = function() {
 					$scope.VM.avatarModal = $uibModal.open({
 			      	templateUrl: 'avatar.html',
 			      	controller: 'avatarInstanceController',
 			      	size: '',
 			      	resolve: {
-			      		test: function() {
-			      			return test;
-			      		}
 			      		}
 	    			});	
 				}
@@ -79,9 +188,8 @@
 				
 			}
 		])
-		.controller('avatarInstanceController', ['$scope', '$uibModalInstance', 'test',
-			function($scope, $uibModalInstance, test) {
-				$scope.test = test;
+		.controller('avatarInstanceController', ['$scope', '$uibModalInstance',
+			function($scope, $uibModalInstance) {
 				$scope.panelOneShow = true;
 				$scope.panelTwoShow = false;
 				$scope.panelThreeShow = false;
@@ -103,6 +211,17 @@
 						$scope.panelThreeShow = true;
 					}
 				}
+				
+				$scope.close = function() {
+					$uibModalInstance.close();
+				}
+			}
+		])
+		
+		.controller('messageInstanceController', ['$scope', '$uibModalInstance', 'messageContent', 'buttonContent',
+			function($scope, $uibModalInstance, messageContent, buttonContent) {
+				$scope.messageContent = messageContent;
+				$scope.buttonContent = buttonContent;
 				
 				$scope.close = function() {
 					$uibModalInstance.close();
