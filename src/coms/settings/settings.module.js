a358531e9528785e5409d39abd066c6db7ac8ad0
huhu213 <15501003885@163.com>
2016-04-05 15:50:19 +0800

    latest version

--- src/coms/settings/settings.module.js
+++ src/coms/settings/settings.module.js
@@ -35,7 +35,9 @@
 					getAllTerms: {method: "GET", url: window.BackendUrl + "/resRestAPI/v1.0/terms"},
 					getAllSubjects: {method: "GET", url: window.BackendUrl + "/resRestAPI/v1.0/subjects"},
 					setUserInfo: {method: "POST", url: window.BackendUrl + "/resRestAPI/v1.0/users/:userid", params: {userid: '@userid'}},
-				})
+					setNewPasswd: {method: "POST", url: window.BackendUrl + "/resRestAPI/v1.0/users/password"},
+					setUserAvatar: {method: "POST", url: window.BackendUrl + "/resRestAPI/v1.0/users/userimage/:userid", params: {userid: '@userid'}}
+                })
 			}
 		])
 		.controller("SettingsController", ['$scope', '$stateParams', '$state', '$location', '$uibModal', 'User','$localStorage',
@@ -52,18 +54,17 @@
 				$scope.selectedSubject;
 				
 				
-				
 				//获取用户信息
 				User.get({
 					userid: $localStorage.authUser.userId
 				}, function(data){
-					var info = data.data;
 					console.log(data);
+					var info = data.data;
 					$scope.userName = info.userName;
 					$scope.schoolName = info.schoolName;
 					$scope.trueName = info.trueName;
 					$scope.sexSelected = info.male;
-					$scope.userImage = info.userImage;
+					$scope.currentAvatar = info.userImage;
 					//获取学段信息
 					User.getAllTerms({
 						
@@ -135,11 +136,13 @@
 				}
 				
 				$scope.setMale = function(male) {
-					$scope.sexSelected = male;
+					if(male) $scope.sexSelected = "男";
+					else $scope.sexSelected = "女";
+					console.log(male);
 				}
 				
 				$scope.saveUserInfo = function() {
-					var saveSuccess = "保存成功";
+					var saveSuccess = "保存成功！";
 					var btnSave = "确定";
 					User.setUserInfo({
 						userid: $localStorage.authUser.userId,
@@ -147,7 +150,7 @@
 						trueName: $scope.trueName,
 						termId: $scope.selectedTerm.id,
 						subjectId: $scope.selectedSubject.id,
-						male: $scope.sexSelected,
+						male: $scope.sexSelected == "男" ? true : false,
 						_method: "PATCH"
 					}, function(data) {
 						if(data) {
@@ -168,9 +171,7 @@
 					})
 				}
 				
-				
-				
-				
+				//修改头像，弹出模态框
 				$scope.changeAvatar = function() {
 					$scope.VM.avatarModal = $uibModal.open({
 			      	templateUrl: 'avatar.html',
@@ -179,12 +180,113 @@
 			      	resolve: {
 			      		}
 	    			});	
+
+	  				var saveSuccess = "";
+	  				var btnSave = "确定";
+
+
+	    			$scope.VM.avatarModal.result.then(function(newpath){
+			  			User.setUserAvatar({
+			  				userid: $localStorage.authUser.userId,
+			  				userImage: newpath
+			  			}, function(data) {
+			  				console.log(data);
+			  				if(data.code == "OK") {
+			  					$scope.currentAvatar = newpath;
+			  					saveSuccess = "头像更换成功！";
+			  				}
+			  				else {
+			  					saveSuccess = "头像更换失败，请重新选取！"
+			  				}
+
+			  				$scope.VM.messageModal = $uibModal.open({
+								templateUrl: "message.html",
+								controller: "messageInstanceController",
+								size: 'sm',
+								resolve: {
+									messageContent: function() {
+										return saveSuccess;
+									},
+									buttonContent: function() {
+										return btnSave;
+									}
+								}
+							})
+			  			});
+				    })
+				}
+
+				$scope.oldPsw = "";
+				$scope.newPsw = "";
+				$scope.confirmNewPsw = "";
+				//确认密码失败
+				$scope.confirmFail = false;
+				//修改密码失败
+				$scope.setPswFail = false;
+				//初始密码错误
+				$scope.oldPswError = false;
+				//修改密码
+				$scope.setPasswd = function() {
+					if($scope.confirmNewPsw !== $scope.newPsw) {
+						$scope.confirmFail = true;
+						$scope.oldPsw = "";
+						$scope.newPsw = "";
+						$scope.confirmNewPsw = "";
+					}
+					else {
+						User.setNewPasswd({
+							oldPassword: $scope.oldPsw,
+							newPassword: $scope.newPsw,
+							_method: "PATCH "
+						}, function(data) {
+							console.log(data);
+							//初始密码错误
+							if(data.code == "InvalidPassword") {
+								$scope.oldPswError = true;
+								$scope.oldPsw = "";
+								$scope.newPsw = "";
+								$scope.confirmNewPsw = "";
+							}
+							//修改成功
+							else if(data.code == "ok") {
+								var saveSuccess = "密码修改成功，下次请用新密码登录！";
+								var btnSave = "确定";
+								$scope.VM.messageModal = $uibModal.open({
+									templateUrl: "message.html",
+									controller: "messageInstanceController",
+									size: 'sm',
+									resolve: {
+										messageContent: function() {
+											return saveSuccess;
+										},
+										buttonContent: function() {
+											return btnSave;
+										}
+									}
+								})
+							}
+							//其他错误
+							else {
+								$scope.setPswFail = true;
+								$scope.oldPsw = "";
+								$scope.newPsw = "";
+								$scope.confirmNewPsw = "";
+							}
+						})
+					}
+				}
+				
+				//重置 
+				$scope.clearPasswd = function() {
+					$scope.oldPsw = "";
+					$scope.newPsw = "";
+					$scope.confirmNewPsw = "";
 				}
 			}
 		])
 		.controller('avatarSettingController', ['$scope', '$uibModal',
 			function($scope, $uibmodal) {
-				//变量共享
+			
 				
 			}
 		])
@@ -193,6 +295,73 @@
 				$scope.panelOneShow = true;
 				$scope.panelTwoShow = false;
 				$scope.panelThreeShow = false;
+
+				$scope.sysAvatarPath = "assets/img/settings/tab_active.png";
+				$scope.selfAvatarPath = "assets/img/settings/tab2.png";
+				
+				
+				//系统头像
+				$scope.systemAvatars = [{
+					path: "assets/img/settings/system_avatar/default.png",
+					clicked: true
+				},
+				{
+					path: "assets/img/settings/system_avatar/Icon0.jpg", 
+					clicked: false
+				},
+				{
+					path: "assets/img/settings/system_avatar/Icon1.jpg", 
+					clicked: false
+				},
+				{
+					path: "assets/img/settings/system_avatar/Icon2.jpg", 
+					clicked: false
+				},
+				{
+					path: "assets/img/settings/system_avatar/Icon3.jpg", 
+					clicked: false
+				},
+				{
+					path: "assets/img/settings/system_avatar/Icon4.jpg", 
+					clicked: false
+				},
+				{
+					path: "assets/img/settings/system_avatar/Icon5.jpg", 
+					clicked: false
+				},
+				{
+					path: "assets/img/settings/system_avatar/Icon6.jpg", 
+					clicked: false
+				},
+				{
+					path: "assets/img/settings/system_avatar/Icon7.jpg", 
+					clicked: false
+				},
+				{
+					path: "assets/img/settings/system_avatar/Icon8.jpg", 
+					clicked: false
+				},
+				{
+					path: "assets/img/settings/system_avatar/Icon9.jpg", 
+					clicked: false
+				},
+				{
+					path: "assets/img/settings/system_avatar/Icon10.jpg", 
+					clicked: false
+				},
+				{
+					path: "assets/img/settings/system_avatar/Icon11.jpg", 
+					clicked: false
+				},
+				{
+					path: "assets/img/settings/system_avatar/Icon12.jpg", 
+					clicked: false
+				},
+				{
+					path: "assets/img/settings/system_avatar/Icon13.jpg", 
+					clicked: false
+				}];
+				
 				
 				$scope.showPanel = function(index) {
 					if(index == 1) {
@@ -210,6 +379,20 @@
 						$scope.panelTwoShow = false;
 						$scope.panelThreeShow = true;
 					}
+				}
+
+				$scope.lastAvatarSelected = 0;
+				$scope.newAvatar = "";
+
+				$scope.selectSystemAvatar = function(index) {
+					$scope.systemAvatars[$scope.lastAvatarSelected].clicked = false;
+					$scope.systemAvatars[index].clicked = true;
+					$scope.lastAvatarSelected = index;
+					$scope.newAvatar = $scope.systemAvatars[index].path;
+				}
+
+				$scope.setAvatar = function() {
+					$uibModalInstance.close($scope.newAvatar);
 				}
 				
 				$scope.close = function() {
