463d67ff6f36f1899f906d8a87c107d2100c414f
zhangly <906005454@qq.com>
2016-04-08 16:00:55 +0800

    src

--- src/coms/res-preview/previewres.module.js
+++ src/coms/res-preview/previewres.module.js
@@ -240,7 +240,6 @@
 							}
 						});
 					}
-					
 				}
 				
 				//获取所有资源
@@ -455,6 +454,8 @@
 						if(data.code=="OK")
 						{
 							$scope.myCommentList = data.data;
+							console.log("我的评论")
+							console.log(data)
 						}else{
 							alert(data.message);
 						}
@@ -468,6 +469,8 @@
 							if(data.code=="OK")
 							{
 								$scope.otherCommentList = data.data;
+								console.log("别人的评论")
+								console.log(data)
 							}else{
 								alert(data.message);
 							}
@@ -509,7 +512,6 @@
 								score++;
 							}
 						});
-						
 						if(score==0)
 						{
 							alert("评完分才能评论哦！");
@@ -526,6 +528,7 @@
 							{
 								getComment($scope.VM.resourceId); //获取我的评论
 								$scope.inputComment="";
+								
 							}else{
 								alert(data.message);
 							}
@@ -558,7 +561,7 @@
 				$scope.deleteCom=function(id){
 					Preview.editComment({
 						commentId:id,
-						_method:"PATCH"
+						_method:"DELETE"
 					}, function(data) {
 						console.log(data)
 						if(data.code=="OK")
