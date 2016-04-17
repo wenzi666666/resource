e1c9dc375fbf447905c09ea641ae91e297778aea
zhangly <906005454@qq.com>
2016-04-17 15:59:16 +0800

    资源定制页接口

--- src/coms/res-custom/customres.module.js
+++ src/coms/res-custom/customres.module.js
@@ -36,7 +36,7 @@
 					},
 					resource:{
 						method: "GET",
-						url: window.BackendUrl + "/api/discuss/home/total"
+						url: BackendUrl + "/resRestAPI/v1.0/customizeRes"
 					}
 				})
 			}
@@ -77,65 +77,6 @@
 				});
 				
 				
-				$scope.VM.resource = [{
-					"subject": "数学",
-					"page": 1,
-					"perPage": 4,
-					"totalLines": 2,
-					"total": 1,
-					item: [{
-						"mtype": "动画",
-						"fromFlag": 0,
-						"thumbnailpath": "http://192.168.111.22:8099/down/get.do?uid=100798967857546&file=TmV3UmVzb3VyY2VCYW5rXERIS1xHWlwwMTAyXEZMMDRcRkwwNDAxXHRmZWR1MDAwMDA4MTU2OVxzdWZmZXIvc3VmZmVyX2ljb24uanBn&sn=tfedu&sign=8e56f7efd94b4f899a1fb616766baaa4&disk=1",
-						"id": 57822,
-						"title": "词汇学习 Suffer",
-						"resCode": "tfedu0000081569",
-					}, {
-						"fromFlag": 0,
-						"thumbnailpath": "http://192.168.111.22:8099/down/get.do?uid=100798967857546&file=TmV3UmVzb3VyY2VCYW5rXERIS1xHWlwwMTAyXEZMMDRcRkwwNDAxXHRmZWR1MDAwMDExNTE0NC90ZmVkdTAwMDAxMTUxNDRfaWNvbi5qcGc%3D&sn=tfedu&sign=2dc56856c32d0b7496c22683dd73193b&disk=1",
-						"id": 75409,
-						"title": " Game: Differences",
-						"resCode": "tfedu0000115144",
-						"mtype": "交互练习",
-					}]
-				}, {
-					"subject": "英语",
-					"page": 1,
-					"perPage": 4,
-					"totalLines": 2,
-					"total": 1,
-					item: [{
-						"fromFlag": 0,
-						"thumbnailpath": "http://192.168.111.22:8099/down/get.do?uid=100798967857546&file=TmV3UmVzb3VyY2VCYW5rXERIS1xHWlwwMTAyXEZMMDRcRkwwNDAxXHRmZWR1MDAwMDExNTE0Ni90ZmVkdTAwMDAxMTUxNDZfaWNvbi5qcGc%3D&sn=tfedu&sign=bac58431ed6e8ee8bd8e6d487d23c8c8&disk=1",
-						"id": 75411,
-						"title": "Game: Differences",
-						"resCode": "tfedu0000115146",
-						"mtype": "动画",
-					}, {
-						"fromFlag": 0,
-						"thumbnailpath": "http://192.168.111.22:8099/down/get.do?uid=100798967857546&file=TmV3UmVzb3VyY2VCYW5rXERIS1xHWlwwMTAyXEZMMDRcRkwwNDAxXHRmZWR1MDAwMDEzOTIzMFx1MSBpbnNwaXJlL2luc3BpcmVfaWNvbi5qcGc%3D&sn=tfedu&sign=260a30955a100ef72f93aa844f50e787&disk=1",
-						"id": 90498,
-						"title": "互动型 词汇学习 Inspire",
-						"resCode": "tfedu0000139230",
-						"mtype": "交互练习",
-					},
-					{
-						"fromFlag": 0,
-						"thumbnailpath": "http://192.168.111.22:8099/down/get.do?uid=100798967857546&file=TmV3UmVzb3VyY2VCYW5rXERIS1xHWlwwMTAyXEZMMDRcRkwwNDAxXHRmZWR1MDAwMDEzOTIzMFx1MSBpbnNwaXJlL2luc3BpcmVfaWNvbi5qcGc%3D&sn=tfedu&sign=260a30955a100ef72f93aa844f50e787&disk=1",
-						"id": 90498,
-						"title": "互动型 词汇学习 Inspire",
-						"resCode": "tfedu0000139230",
-						"mtype": "交互练习",
-					},{
-						"fromFlag": 0,
-						"thumbnailpath": "http://192.168.111.22:8099/down/get.do?uid=100798967857546&file=TmV3UmVzb3VyY2VCYW5rXERIS1xHWlwwMTAyXEZMMDRcRkwwNDAxXHRmZWR1MDAwMDEzOTIzMFx1MSBpbnNwaXJlL2luc3BpcmVfaWNvbi5qcGc%3D&sn=tfedu&sign=260a30955a100ef72f93aa844f50e787&disk=1",
-						"id": 90498,
-						"title": "互动型 词汇学习 Inspire",
-						"resCode": "tfedu0000139230",
-						"mtype": "交互练习",
-					}]
-
-				}];
 
 				console.log($scope.VM.resource);
 				
