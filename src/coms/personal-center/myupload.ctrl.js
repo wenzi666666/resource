5f7f43cf99718f10018ff1f7baa72a8480bbd48b
huhu213 <15501003885@163.com>
2016-04-19 17:10:46 +0800

    previreRes

--- src/coms/personal-center/myupload.ctrl.js
+++ src/coms/personal-center/myupload.ctrl.js
@@ -1,3 +1,4 @@
+
 /**
  * 个人中心模块
  */
@@ -113,7 +114,7 @@
 				$scope.previewRes = function(res) {
 					//已转换为可预览格式
 					if(res.isFinished) {
-						$state.go('previewres', {resId:list.resId,curTfcode:'',fromFlag:1,search:'person',type:'1'});
+						$state.go('previewres', {resId:list.resId,curTfcode:'',fromFlag:1,search:'person',type:'1',back: 'upload'});
 					}
 					else {
 						ModalMsg.alert("该资源正在转码中，暂时不能预览，请稍后重试！");
