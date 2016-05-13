2824b571984ef0922b385cd46c777e05af8abc45
lgzhang <zhanglg@tfedu.net>
2016-05-13 09:44:54 +0800

    fix reslist

--- src/coms/res-system/systemres.module.js
+++ src/coms/res-system/systemres.module.js
@@ -284,6 +284,7 @@
 					console.log("received:",d)
 					// 列出资源
 					getResList(d);
+					tmpCtrl = true;
 					// 列出  资源类型和格式
 					$scope.typeAndFormat(0, 0);
 					// 更改目录标题
@@ -349,7 +350,7 @@
 				var mTypeId = 0;
 				var format = "全部";
 				$scope.orderBy = 0;
-				// 页面刷新时 控制list 不加载
+				// 页面刷新时和点击目录树时  控制list 不加载 
 				var tmpCtrl = true;
 				$scope.typeAndFormat = function(poolId, typeId){
 					// 设值
