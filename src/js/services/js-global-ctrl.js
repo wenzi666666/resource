53dcc587995594361c5a941269cc46d2f6462a3d
lgzhang <zhanglg@tfedu.net>
2016-06-14 10:32:36 +0800

    fix prepare statis

--- src/js/services/js-global-ctrl.js
+++ src/js/services/js-global-ctrl.js
@@ -125,7 +125,7 @@
 		var browser=navigator.appName 
 		var b_version=navigator.appVersion 
 		var version=b_version.split(";"); 
-		var trim_Version=version[1].replace(/[ ]/g,""); 
+		var trim_Version=version[1]?version[1].replace(/[ ]/g,""):''; 
 		if(browser=="Microsoft Internet Explorer" && trim_Version=="MSIE9.0") 
 		{ 
 //			alert("IE 9.0"); 
